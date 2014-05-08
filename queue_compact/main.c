9672b3f14be039b3a0bcd8b00008dbdcb8ff3319
Bart Bilos <anton.bilos@gmail.com>
2014-05-08 16:08:33 +0200

    * fixed problem, no init of i

--- queue_compact/main.c
+++ queue_compact/main.c
@@ -18,7 +18,7 @@ int dataqueue[QUEUESIZE] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
 
 int main()
 {
-    int i;
+    int i = 0;
     //int *queuedata;
     void * dataout;
     //void * dummyqueuezero = &dataqueue[1];
